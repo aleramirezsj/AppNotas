365ee0346e48e90b52af088a62ddfd255ef2328d
aleramirezsj <aleramirezsj@gmail.com>
2023-10-19 14:41:07 -0300

    pantalla iniciar sesion creada (faltan ajustes)

---
+++ AppNotas/Views/Notas/IniciarSesionView.xaml.cs
@@ -0,0 +1,14 @@
+namespace AppNotas.Views.Notas;
+
+public partial class IniciarSesionView : ContentPage
+{
+	public IniciarSesionView()
+	{
+		InitializeComponent();
+	}
+
+    private void btnIniciarSesion_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
